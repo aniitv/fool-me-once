c06297bfefc63f73fd334d6c94fcb005332d29a7
Ro-sya <yaroslavasemenyuk12@gmail.com>
2026-02-20 15:33:32 +0200

    Add cards

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/index.css";
+import Login from "./components/Login.jsx";
+import Logo from "./assets/icons/logo.png";
+
+function App() {
+  return (
+    <div>
+      <img src={Logo} alt="Logo" className="logo" />
+      <Login />
+    </div>
+  );
+}
+export default App;
