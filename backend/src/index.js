c00df7ec156cbd6c91470dad6e92e8f93e7bfc44
Anastasiia Zotova <anastasiiaazotova@gmail.com>
2026-03-26 20:22:49 +0200

    remove old files

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173", // Дозволяємо ТІЛЬКИ твій React
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
     credentials: true,
   }),
