ae2a142fc1e86559bc4c820a5d21abba30f1aa5f
Anastasiia Zotova <anastasiiaazotova@gmail.com>
2026-03-27 14:30:18 +0200

    chose one way(LRU)

--- backend/src/memoize.js
+++ backend/src/memoize.js
@@ -1,8 +1,4 @@
-export function memoize(fn, options = {}) {
-  const limit = options.limit || Infinity;
-  const policy = options.policy || "LRU";
-  const ttl = options.ttl || 60000;
-
+export function memoize(fn, limit = 10) {
   const cache = new Map();
 
   return function (...args) {
@@ -11,49 +7,29 @@ export function memoize(fn, options = {}) {
 
     if (cache.has(key)) {
       const data = cache.get(key);
-
-      if (policy === "Time" && now - data.createdAt > ttl) {
-        cache.delete(key);
-      } else {
-        data.usageCount++;
-        data.lastUsed = now;
-        return data.result;
-      }
+      data.lastUsed = now;
+      return data.result;
     }
 
     const result = fn(...args);
 
     if (cache.size >= limit) {
+      let oldestTime = Infinity;
       let keyToRemove = null;
 
-      if (policy === "LRU") {
-        let oldestTime = Infinity;
-        for (let [itemKey, itemData] of cache.entries()) {
-          if (itemData.lastUsed < oldestTime) {
-            oldestTime = itemData.lastUsed;
-            keyToRemove = itemKey;
-          }
+      for (let [itemKey, itemData] of cache.entries()) {
+        if (itemData.lastUsed < oldestTime) {
+          oldestTime = itemData.lastUsed;
+          keyToRemove = itemKey;
         }
-      } else if (policy === "LFU") {
-        let smallestUsage = Infinity;
-        for (let [itemKey, itemData] of cache.entries()) {
-          if (itemData.usageCount < smallestUsage) {
-            smallestUsage = itemData.usageCount;
-            keyToRemove = itemKey;
-          }
-        }
-      } else {
-        keyToRemove = cache.keys().next().value;
       }
 
       if (keyToRemove) cache.delete(keyToRemove);
     }
 
     cache.set(key, {
-      result: result,
-      createdAt: now,
-      lastUsedAt: now,
-      usageCount: 1,
+      result,
+      lastUsed: now,
     });
 
     return result;
