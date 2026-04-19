d872d241c2d40886e10d3419a9cff053008ffd7c
Ro-sya <yaroslavasemenyuk12@gmail.com>
2026-04-19 14:36:01 +0300

    Delete api

--- backend/services/aiService.js
+++ backend/services/aiService.js
@@ -1,5 +1,5 @@
 async function fetchAi(cards) {
-  const response = await fetch("AIzaSyAKFVyqzmRLqZS6ykJnqgShLM_mDVKForw", {
+  const response = await fetch("http://localhost:5000/api/ai/interpret", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
